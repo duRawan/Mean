a51d69e0dc77d2700dfb3e20933dbb055ae84958
duRawan <du_rawan@outlook.com>
2019-10-03 12:52:50 +0300

    cars and cats with views

--- cars_and_cats_Ex/server.js
+++ cars_and_cats_Ex/server.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const app = express();
 app.listen(8000, () => console.log("listening on port 8000"));
-<<<<<<< HEAD
 app.use(express.static(__dirname + "/static"));
-=======
 app.use(express.static(__dirname + "/static"));
->>>>>>> de87d3f3880fa1061a605effb778efe3b7dd7b6d
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.get("/cats", (req, res) => {
+    // hard-coded user data
+ 
+    res.render('cats');
+ })
+ app.get("/cars", (req, res) => {
+    // hard-coded user data
+ 
+    res.render('cars');
+ })
+ app.get("/cars/new", (req, res) => {
+    // hard-coded user data
+ 
+    res.render('NewCar');
+ })
