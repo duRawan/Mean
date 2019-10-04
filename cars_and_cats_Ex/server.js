62a6ec6aba49494814ca3edeb15645f9d9a7ae6c
duRawan <du_rawan@outlook.com>
2019-10-04 16:07:53 +0300

    cats data assignment

--- cars_and_cats_Ex/server.js
+++ cars_and_cats_Ex/server.js
@@ -1,22 +1,35 @@
 const express = require("express");
 const app = express();
 app.listen(8000, () => console.log("listening on port 8000"));
-app.use(express.static(__dirname + "/static"));
-app.use(express.static(__dirname + "/static"));
-app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + "/static"));//html pages 
+app.set('view engine', 'ejs');//ejs in the views 
+app.set('views', __dirname + '/views');//after setting we will get the information from the page
 app.get("/cats", (req, res) => {
-    // hard-coded user data
- 
     res.render('cats');
  })
  app.get("/cars", (req, res) => {
-    // hard-coded user data
- 
     res.render('cars');
  })
  app.get("/cars/new", (req, res) => {
-    // hard-coded user data
- 
     res.render('NewCar');
  })
+ app.get("/cuddle", (req, res) => {
+     var cat_data=[
+         {name:'cuddle',food:'spagetti',age:3,sleep_spot:'under the bed',num:1}
+     ];
+    res.render('details', {cat: cat_data});
+ })
+ app.get("/jami", (req, res) => {
+    var cat_data=[
+        {name:'jami',food:'beans',age:2,sleep_spot:'under the window',num:2}
+    ];
+   res.render('details', {cat: cat_data});
+})
+app.get("/zac", (req, res) => {
+    var cat_data=[
+        {name:'zac',food:'tona',age:1,sleep_spot:'on the fridge',num:3}
+    ];
+   res.render('details', {cat: cat_data});
+})
+
+ 
