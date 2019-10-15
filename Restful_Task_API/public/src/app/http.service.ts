12f71154f2f5039893d210c1dc94129061842965
duRawan <du_rawan@outlook.com>
2019-10-15 18:15:43 +0300

    Restful Tasks-Fetch data

---
+++ Restful_Task_API/public/src/app/http.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  constructor(private _http: HttpClient){
+    this.getTasks();
+    this.deleteTasks();
+  }
+  getTasks(){
+    // our http response is an Observable, store it in a variable
+       let tempObservable = this._http.get('/tasks');
+       let tempObservable_ = this._http.get('/tasks/5da5d29904acf7067e85641f');
+    // subscribe to the Observable and provide the code we would like to do with our data from the response
+       tempObservable.subscribe(data => console.log("Got our tasks!", data));
+       tempObservable_.subscribe(data => console.log("Got our task by id!", data));
+    }
+    deleteTasks(){
+      // our http response is an Observable, store it in a variable
+  
+      // subscribe to the Observable and provide the code we would like to do with our data from the response
+         
+         let tempObservable = this._http.delete('/tasks/5da5cd6e04acf7067e85641c');
+         tempObservable.subscribe(data => console.log("delete our task by id!", data));
+      }
+}
--- Restful_Task_API/server.js
+++ Restful_Task_API/server.js
@@ -4,7 +4,7 @@ var bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());// for responding with json 
-
+app.use(express.static( __dirname + '/public/dist/public' ));
 require('./server/config/mongoose.js');
 var routes_setter = require('./server/config/routes.js');
 routes_setter(app);
