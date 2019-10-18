3942ff8d49ff11af9d58d57bd8b397a4f4b12ade
duRawan <du_rawan@outlook.com>
2019-10-18 16:03:36 +0300

    Restful Tasks Interactive

--- Restful_Task_API/public/src/app/app.component.ts
+++ Restful_Task_API/public/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component , OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpService } from './http.service';
 @Component({
   selector: 'app-root',
@@ -8,19 +8,37 @@ import { HttpService } from './http.service';
 // import and Implement OnInit.
 export class AppComponent implements OnInit {
   title = 'Rawan!';
-  constructor(private _httpService: HttpService){}
- 
+  constructor(private _httpService: HttpService) { }
+
   // ngOnInit will run when the component is initialized, after the constructor method.
-  tasks=[];
-  ngOnInit(){
-    this.getTasksFromService();
- 
+  tasks = [];
+  Onetask=[];
+  ngOnInit() {
+    //this.getTasksFromService();
+
   }
-  getTasksFromService(){
-   let observable= this._httpService.getTasks();
-   observable.subscribe(data => {console.log("Got your our tasks", data)
-   this.tasks = data['task'];
-  //console.log(this.tasks)
-});
+  getTasksFromService() {
+    let observable = this._httpService.getTasks();
+    observable.subscribe(data => {
+      console.log("Got your our tasks", data)
+      this.tasks = data['task'];
+      //console.log(this.tasks)
+    });
   }
+  getTaskDetails(id: string) {
+    console.log(id);
+    let observable_ = this._httpService.getOneTask(id);
+    observable_.subscribe(data => {
+      console.log("Got your our task", data)
+      this.Onetask = data['task'];
+      console.log(this.Onetask)
+    });
+  }
+  onButtonClickParam(num: Number): void {
+    console.log(`Click event is working with num param: ${num}`);
+    // call the service's method to post the data, but make sure the data is bundled up in an object!
+    let observable = this._httpService.postToServer({ data: num });
+    observable.subscribe(data => console.log("Got our data!", data));
+  }
+
 }
--- Restful_Task_API/public/src/app/http.service.ts
+++ Restful_Task_API/public/src/app/http.service.ts
@@ -5,25 +5,34 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HttpService {
 
-  constructor(private _http: HttpClient){
+  constructor(private _http: HttpClient) {
     // this.getTasks();
     // this.deleteTasks();
   }
-  getTasks(){
+  getTasks() {
     // our http response is an Observable, store it in a variable
-      //  let tempObservable = this._http.get('/tasks');
-      //  let tempObservable_ = this._http.get('/tasks/5da5d29904acf7067e85641f');
+    //  let tempObservable = this._http.get('/tasks');
+    //  let tempObservable_ = this._http.get('/tasks/5da5d29904acf7067e85641f');
     // subscribe to the Observable and provide the code we would like to do with our data from the response
-      //  tempObservable.subscribe(data => console.log("Got our tasks!", data));
-      //  tempObservable_.subscribe(data => console.log("Got our task by id!", data));
-      return this._http.get('/tasks');
-    }
-    deleteTasks(){
-      // our http response is an Observable, store it in a variable
-  
-      // subscribe to the Observable and provide the code we would like to do with our data from the response
-         
-         let tempObservable = this._http.delete('/tasks/5da5cd6e04acf7067e85641c');
-         tempObservable.subscribe(data => console.log("delete our task by id!", data));
-      }
+    //  tempObservable.subscribe(data => console.log("Got our tasks!", data));
+    //  tempObservable_.subscribe(data => console.log("Got our task by id!", data));
+    return this._http.get('/tasks');
+  }
+  // deleteTasks() {
+  //   // our http response is an Observable, store it in a variable
+
+  //   // subscribe to the Observable and provide the code we would like to do with our data from the response
+
+  //   let tempObservable = this._http.delete('/tasks/5da5cd6e04acf7067e85641c');
+  //   tempObservable.subscribe(data => console.log("delete our task by id!", data));
+  // }
+  getOneTask(num) {
+    return this._http.get('/tasks/'+num);
+  }
+  postToServer(num) {
+    // use the .post() method of HttpClient
+    // num must be an object
+    // provide the url of your post route - make sure this is set up in your server!
+    return this._http.post('/tasks', num);
+  }
 }
